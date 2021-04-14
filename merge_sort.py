719ba6e1580ec0256bc419aae3cb10a33c9c8f79
Richard Donald <richard_donald92@outlook.com>
2021-04-14 13:39:14 -0400

    Add files via upload
    Completed merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -19,7 +19,6 @@ def merge_sort(lst):
         return lst
     
     # divide using a helper function split()
-    
     left_half, right_half = split(lst)
     
     # conquer step
@@ -42,3 +41,55 @@ def split(lst):
     right_lst = lst[mid_index:]
     
     return left_lst, right_lst
+
+
+def merge(left, right):
+    """
+    This function merges two lists (arrays) , sorting them in the process.
+    Return a new merged list.
+    """
+    # merged list
+    l = []
+    # index in left list
+    i = 0
+    # index in right list
+    j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            l.append(left[i])
+            i += 1
+        else:
+            l.append(right[j])
+            j += 1            
+            
+    
+    while i < len(left):
+        l.append(left[i])
+        i += 1
+        
+    while j < len(right):
+        l.append(right[j])
+        j += 1
+
+    return l
+            
+
+def verify_sorted(lst):
+    n = len(lst)
+    if n == 0 or n == 1:
+        return True
+    
+    return lst[0] < lst[1] and verify_sorted(lst[1:])
+
+
+my_list = [54, 78, 97, 5, 16, 43, 9, 12, 4]
+
+l = merge_sort(my_list)
+
+print(l)
+print(verify_sorted(my_list))      
+print(verify_sorted(l))      
+
+            
+            
